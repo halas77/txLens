0b71f6be23ec00702d96eb3a87343bce675cd9b5
Dawit Mellese <dawitm777@gmail.com>
2025-03-03 16:41:03 +0300

    update backend configuration and frontend API endpoint; add .env.example and build script

--- backend/app.ts
+++ backend/app.ts
@@ -16,7 +16,7 @@ app.use("/api/tx-data", tnxRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on ${PORT}`);
 });
 
 export default app;

393ff9913d8e438f7aa1aeca40b2970537465b18
Dawit Mellese <dawitm777@gmail.com>
2025-03-04 15:54:27 +0300

    update SUPPORTED_DOMAINS to include basescan.org and fix API endpoint URL in fetchSummary function

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -15,4 +15,4 @@ export const mockData = {
 export const customScroll =
   "[&::-webkit-scrollbar]:h-1 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-300 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-400";
 
-export const SUPPORTED_DOMAINS = ["etherscan.io", "lisk.com"];
+export const SUPPORTED_DOMAINS = ["etherscan.io", "lisk.com", "basescan.org",];
